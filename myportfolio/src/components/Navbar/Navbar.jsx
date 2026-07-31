99157987a90e47d7ffba2df985776500b371083c
hluleko <mchavalala55@gmail.com>
2026-07-31 03:46:15 +0200

    Navbar updates

--- myportfolio/src/components/Navbar/Navbar.jsx
+++ myportfolio/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className="bg-transparent">
+        <div className="text-lg font-semibold cursor-pointer">
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white">Musa</span>
+          <span className="text-[#8245ec]">/</span>
+          <span className="text-white">Chavalala</span>
+          <span className="text-[#8245ec]">&gt;</span>
+        </div>
       
-    </div>
+    </nav>
   )
 }
 
