d7ad6500541d69b8ff5af8852f7ab9b2af374fa1
hluleko <mchavalala55@gmail.com>
2026-08-06 04:29:47 +0200

    cv update

--- myportfolio/src/components/About/About.jsx
+++ myportfolio/src/components/About/About.jsx
@@ -55,7 +55,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            href="https://drive.google.com/file/d/1EQA3lbDOcwpPbzLTg2jNlglmFKdwhNF_/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
