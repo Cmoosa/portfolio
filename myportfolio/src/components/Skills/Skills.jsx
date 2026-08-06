f57f90dd1ffdae928ca7a6111586db3c73019e1d
hluleko <mchavalala55@gmail.com>
2026-08-06 04:50:04 +0200

    skills update

--- myportfolio/src/components/Skills/Skills.jsx
+++ myportfolio/src/components/Skills/Skills.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { SkillsInfo } from '../../constants'
 
 const Skills = () => {
   return (
-    <div>
+    <section
+     id="skills"
+     className='py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient'>
       
-    </div>
+    </section>
   )
 }
 
